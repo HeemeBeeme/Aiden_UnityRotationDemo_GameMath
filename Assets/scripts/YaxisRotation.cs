e51cc6adca6dfdef652e6665306de67b2f76f79a
Heeme Beeme <heemebeeme@gmail.com>
2025-11-06 12:59:52 -0400

    object created with y axis rotation
    a script rotates the object using a sine wave

---
+++ Assets/scripts/YaxisRotation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class YaxisRotation : MonoBehaviour
+{
+
+    public float Amplitude = 1.0f;
+    public float Frequency = 1.0f;
+    public float TimeElapsed = 0.0f;
+
+    public int Speed = 1;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, (Amplitude * Mathf.Sin(Frequency * TimeElapsed) + 2), 0);
+
+        TimeElapsed += Time.deltaTime * Speed;
+    }
+}
